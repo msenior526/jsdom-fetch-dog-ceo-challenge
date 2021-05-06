7b6c90d7f7efccb88f56b7a88fa411b78b29deb5
msenior526 <msenior526@gmail.com>
2021-05-06 14:41:48 -0400

    commit tree 88d871c39860bc80c940cd281d0a358a58109d61

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 console.log('%c HI', 'color: firebrick')
+window.addEventListener('DOMContentLoaded', () => {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => {
+        const imageContainer = document.getElementById('dog-image-container');
+        json.message.forEach((element) => {
+            const image = document.createElement('img');
+            image.src = element;
+            imageContainer.appendChild(image);
+        })
+    })
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(resp => resp.json())
+    .then(json => {
+        const dogBreedList = document.getElementById('dog-breeds');
+        for ( const key in json.message) {
+            const listItem = document.createElement('li');
+            listItem.innerText = key;
+            listItem.addEventListener('click', (e) => {
+                listItem.style.color = "red";
+            })
+            dogBreedList.appendChild(listItem);
+        }
+    })
+})
+
+
